51a784635be0efaae13ef5e7259dcb791b43bc32
Anna Lotko <annaklotko@gmail.com>
2016-10-28 12:27:50 -0600

    added rough wireframes to a new folder

--- q1_project_test/js/canvas_composition.js
+++ q1_project_test/js/canvas_composition.js
@@ -2,12 +2,12 @@
 $(function() {
 
   var canvas = $("#canvas");
-  var context = canvas.getContext('2d');
+  // var context = canvas.getContext('2d');
 
 
   // load the stored image and append to main container
   var selectedImageObj = localStorage.getItem("image_selection");
-  // $("#photo_container").append('<img src=' + selectedImageObj + 'alt="selected_image" class="selected_image" name="selected_image">');
+  $("#photo_container").append('<img src=' + selectedImageObj + 'alt="selected_image" class="selected_image" name="selected_image">');
 
-  context.drawImage(selectedImageObj);
+  // context.drawImage(selectedImageObj);
 });

b325d9a139103080b96aeb0b5101391c1e27b620
Anna Lotko <annaklotko@gmail.com>
2016-10-28 15:49:45 -0600

    saving image to downloads from p5js working / last commit before switching to unsplash

--- q1_project_test/js/sketch.js
+++ q1_project_test/js/sketch.js
@@ -24,9 +24,37 @@ function imageFailed() {
 }
 
 function setup() {
-
+  // createCanvas(1875, 1275);
+  createCanvas(800, 600);
+  background(51);
 }
 
 function draw() {
   image(img, 0, 0);
+}
+
+function mouseClicked() {
+  console.log("clicked");
+  $.ajax({
+    // type: "POST",
+    // url: 'https://api.lob.com/v1/addresses/test_97f0caa8c52f230f7bef2daef8b58e70f81',
+    // data: {
+    //   "id": "adr_f4540ef741dd2b23",
+    //   "description": null,
+    //   "name": "Anna Lotko",
+    //   "company": null,
+    //   "phone": null,
+    //   "email": null,
+    //   "address_line1": "1260 Blala Avenue",
+    //   "address_line2": "Unit 15",
+    //   "address_city": "Smolder",
+    //   "address_state": "Colorado",
+    //   "address_zip": "80304",
+    //   "address_country": "United States",
+    //   "metadata": {},
+    //   "date_created": "2016-10-28T21:25:23.688Z",
+    //   "date_modified": "2016-10-28T21:25:23.688Z",
+    //   "object": "address"
+    // },
+  });
 }

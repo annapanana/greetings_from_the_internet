5b085a191446c45dd58faf0f2d4545db44e62d9f
Anna Lotko <annaklotko@gmail.com>
2016-10-29 10:07:44 -0600

    deployed to surge and added CNAME

--- q1_project_test/js/sketch.js
+++ q1_project_test/js/sketch.js
@@ -1,12 +1,5 @@
 "use strict";
 
-// $(function() {
-//
-//   // load the stored image and append to main container
-//   var selectedImageObj = localStorage.getItem("image_selection");
-//   $("#photo_container").append('<img src=' + selectedImageObj + 'alt="selected_image" class="selected_image" name="selected_image">');
-//
-// });
 var img;
 function preload() {
   var selectedImageURL = localStorage.getItem("image_selection");
